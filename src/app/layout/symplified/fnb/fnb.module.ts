4d6894343437ad648a1a267f1148734fe692b748
Ahmad Miqdaad <ahmad.miqdaad@kalsym.com>
2022-01-09 07:44:31 +0800

    bug fix

--- src/app/layout/symplified/fnb/fnb.module.ts
+++ src/app/layout/symplified/fnb/fnb.module.ts
@@ -4,21 +4,22 @@ import { SharedModule } from 'app/shared/shared.module';
 import { FnbLayoutComponent } from 'app/layout/symplified/fnb/fnb.component';
 import { MatIconModule } from '@angular/material/icon';
 import { BreadcrumbModule } from 'app/layout/common/breadcrumb/breadcrumb.module';
-import { Error404Module } from 'app/shared/error/error-404/error-404.module';
+import { Error404Component } from 'app/shared/error/error-404/error-404.component';
 
 @NgModule({
     declarations: [
-        FnbLayoutComponent
+        FnbLayoutComponent,
+        Error404Component
     ],
     imports     : [
         RouterModule,
         SharedModule,
         BreadcrumbModule,
         MatIconModule,
-        Error404Module
     ],
     exports     : [
-        FnbLayoutComponent
+        FnbLayoutComponent,
+        Error404Component
     ]
 })
 export class FnbLayoutModule
